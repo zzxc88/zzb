6dd1bc97e7650428c38d3aac23982953fe668222
zzxc88 <rcxxrcxx3@gmail.com>
2025-07-27 20:34:55 +0700

    he

---
+++ settings/config.js
@@ -0,0 +1,8 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.ownername = ""
+global.owner = ['']
+global.thumb = ""
+global.botname = "MikasaChan"
+global.packname = "MikasaChan"
+global.namaCreator = "GxყҽɳɳOfc"
+global.isLink = ''
